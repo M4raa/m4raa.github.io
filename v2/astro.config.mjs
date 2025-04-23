384010821621ba68354e40e777dbb7d0157f5a5a
David Maratrat <dmaratrat@itbacking.com>
2025-04-23 12:09:52 +0200

    Desarrollo Front -> Build 7

--- v2/astro.config.mjs
+++ v2/astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   base: '/',
-  outDir: '../docs', 
+  outDir: '/docs',
   vite: {
     plugins: [tailwindcss()],
   },
