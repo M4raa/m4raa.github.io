20462bc91ec99a0777ca73b4ede1721b2dbce622
David Maratrat <dmaratrat@itbacking.com>
2025-04-23 10:22:11 +0200

    Desarrollo Front -> Reestructuración 3

--- v2/astro.config.mjs
+++ v2/astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/m4ra.github.io/', // 👈 importante si usas gh-pages y no un dominio personalizado
+  base: '/m4raa.github.io/', // 👈 importante si usas gh-pages y no un dominio personalizado
   outDir: '../docs', 
   vite: {
     plugins: [tailwindcss()],
